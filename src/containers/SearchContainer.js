ef8ea05060acf37e61f96b7aa7dbabb847da2e99
Kameron Seto <tan@Sad-Panda.local>
2019-01-13 19:51:53 -0800

    search refactored

--- src/containers/SearchContainer.js
+++ src/containers/SearchContainer.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import Search from './../components/Search.js';
 import { connect } from 'react-redux';
-import handleSearchChange from '../actions/search.js';
-var SearchContainer = () => {};
+import Search from './../components/Search.js';
+import handleVideoSearch from '../actions/search.js';
 
-//TODO: define a SearchContainer component which will hook up your action
-// dispatchers with your search component props.
-//HINT: use react-redux 'connect' method to generate a container component from
-//state and dispatch mappings.
+var mapStateToProps = () => {};
+
+var mapDispatchToProps = (dispatch) => ({
+  handleVideoSearch: (q) => dispatch(handleVideoSearch(q)),
+});
+
+var SearchContainer = connect(mapStateToProps, mapDispatchToProps)(Search);
 
 export default SearchContainer;
